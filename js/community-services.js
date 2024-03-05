609597542284117169038a443bc35aab3ee7a8e9
Ahmad Mushawwir <mushawwirahmad45@gmail.com>
2024-03-05 21:56:43 +0700

    add content

--- js/community-services.js
+++ js/community-services.js
@@ -11,35 +11,19 @@ document.addEventListener("DOMContentLoaded", function () {
         {
             title: "SIWARAS",
             tipe: "Website Project",
-            description: "",
+            description: "The Healthy Citizen Information System (SIWARAS) is an information system designed to manage and store data on patients who come for health checks. This system allows health workers to record patient information and view monthly patient summary data.",
             url: "Check it out 👉",
             anchor: "https://siwaras.psti.unisayogya.ac.id/",
             imgUrl: "./images/rcs-2.png"
         },
         {
-            title: "LAZISMU",
-            tipe: "Website Project",
-            description: "....",
-            url: "Check it out 👉",
-            anchor: "https://pijar-travelling.netlify.app/",
-            imgUrl: "./images/project-img1.3.png"
-        },
-        {
             title: "Satu lagi",
             tipe: "Website Project",
-            description: "",
+            description: "This product is an aid to assist in the elimination of defecation (defecation) in patients following hip fracture surgery. More specifically, this invention is equipped with an aid system to adjust the range of motion (flexion-extension) which can provide comfort in movement in the range of motion (flexion-extension). movement during the defecation elimination process so that it can prevent dislocation of post-operative hip fracture patients. In the concept process for making a flexible chair, discussions have been carried out on making this tool from the perspective of academics and health practitioners according to the fields, namely Nursing and Physiotherapy.",
             url: "Check it out 👉",
-            anchor: "http://aeac.psti.unisayogya.ac.id",
-            imgUrl: "./images/project-img1.5.png"
+            anchor: "https://pijar-travelling.netlify.app/",
+            imgUrl: "./images/rcs-3.png"
         },
-        {
-            title: "Pivot Table, Dashboard, etc of Sales Data",
-            tipe: "Data Analysis Project",
-            description: "Student made this project through an assignment student did from Microsoft about Data Analyst using Excel. This project taught student to make pivot tables, pivot charts, dashboards, hierarchical charts, etc. to help companies achieve their goals based on data owned by the company within a certain period of time.",
-            url: "Check it out 👉",
-            anchor: "https://github.com/ahmad14022/sales-data-1/blob/main/README.md",
-            imgUrl: "./images/project-img1.6.png"
-        }
     ];
 
     // Function to generate project cards
